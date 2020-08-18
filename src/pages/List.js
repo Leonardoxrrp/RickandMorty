a98a223bff748af2a1604f0ac18771e002e4e334
leonardoxrrp <leonardoxrrp@gmail.com>
2020-08-18 23:59:33 +0200

    Pagination works !

--- src/pages/List.js
+++ src/pages/List.js
@@ -12,24 +12,25 @@ class List extends Component {
         this.fetchCharacter()
     }
     fetchCharacter = async (type) => {
-        this.setState({loading:true})
         let typeValue = 1;
+        this.setStae=({loading: true})
 
         if (type === 1) {
-            typeValue = this.state.page +1;
+            typeValue = this.state.page + 1;
         }
         else if (type === 0) {
-            typeValue = this.state.page -1;
+            typeValue = this.state.page - 1;
 
         }
+        this.setState({loading: true})
         try {
-            const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${typeValue}`)
-            const data = await response.json()
-            this.setState({
-                character: data.results,
-                loading: false,
-                page: typeValue,
-            })
+                const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${typeValue}`)
+                const data = await response.json()
+                this.setState({
+                    character: data.results,
+                    loading: false,
+                    page: typeValue,
+                }) 
 
         } catch (error) {
             console.log(error)
@@ -43,46 +44,49 @@ class List extends Component {
                 <div className="header-container">
                     <div className="container">
                         <div className="title">
-                        <h1>Rick and Morty</h1>
+                            <h1>Rick and Morty</h1>
                         </div>
                     </div>
                 </div>
                 <div className="body-container">
-                <div className="container">
-                    <div className="row buttons-container">
-                    {this.state.loading === false && this.state.page > 1 &&
-                    
-                    (
-                    <button onClick={()=> this.fetchCharacter(0)} className="btn btn-success previous">Previous</button>
-                        )}
-                 {this.state.loading === false &&
-                    (
-                    <button onClick={()=> this.fetchCharacter(1)} className="btn btn-success next">Next</button>
-                        )}
+                    <div className="container">
+                        <div className="row buttons-container">
+                            {this.state.page > 1 &&
 
-                    </div>
-                    <div className="row">
-                        {this.state.character.map(character =>
-                            <div className="card col-12 col-md-6 col-lg-4 col-xl-3" key={character.id}>
-                                <img className="card-img-top" src={character.image} alt="Card image" />
-                                <div className="card-body">
-                                    <h5 className="card-title card-name">{character.name}</h5>
-                                    {character.status === "Alive" && (<h5 className="card-text badge badge-success">Alive</h5>)}
-                                    {character.status === "Dead" && (<h5 className="card-text badge badge-danger ">Dead</h5>)}
-                                    {character.status === "unknown" && (<h5 className="card-text badge badge-dark">Unknown</h5>)}
-                                    <p className="card-text badge badge-light">{character.species}.</p>
+                                (
+                                    <button onClick={() => this.fetchCharacter(0)} className="btn btn-success previous">Previous</button>
+                                )}
+                                    <p className="pageNumber">Page: {this.state.page}</p>
+                            {this.state.loading === false && this.state.page <34 &&
+                                (
+                                    <button onClick={() => this.fetchCharacter(1)} className="btn btn-success next">Next</button>
+                                )}
+
+                        </div>
+                        <div className="row">
+                            {this.state.character.map(character =>
+                                <div className="card col-12 col-md-6 col-lg-4 col-xl-3" key={character.id}>
+                                    <img className="card-img-top" src={character.image} alt="Card image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title card-name">{character.name}</h5>
+                                        {character.status === "Alive" && (<h5 className="card-text badge badge-success">Alive</h5>)}
+                                        {character.status === "Dead" && (<h5 className="card-text badge badge-danger ">Dead</h5>)}
+                                        {character.status === "unknown" && (<h5 className="card-text badge badge-dark">Unknown</h5>)}
+                                        <p className="card-text badge badge-light">{character.species}.</p>
+                                    </div>
                                 </div>
+                            )}
+
+                        </div>
+                    </div>
+                    {this.state.loading === true &&
+                            <div className="loader">
+                                <Loader />
                             </div>
-                        )}
-
-                    </div>
-                    <div className="loader">
-                        {this.state.loading && <Loader />}
-                    </div>
+                        }
 
                 </div>
-                </div>
-               
+
 
             </React.Fragment>
         )
