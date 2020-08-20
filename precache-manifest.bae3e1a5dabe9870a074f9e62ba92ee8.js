a5efaab880fae56d45c66a29d5766e387e818b6a
leonardoxrrp <leonardoxrrp@gmail.com>
2020-08-21 00:02:37 +0200

    Updates

---
+++ precache-manifest.bae3e1a5dabe9870a074f9e62ba92ee8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba58d3bb3459e2290b6f55ea9ca7c005",
+    "url": "/RickandMorty/index.html"
+  },
+  {
+    "revision": "4193aac8cbd973a4acd8",
+    "url": "/RickandMorty/static/css/2.b8fb372d.chunk.css"
+  },
+  {
+    "revision": "dc42a3427a272312a858",
+    "url": "/RickandMorty/static/css/main.c85fa52b.chunk.css"
+  },
+  {
+    "revision": "4193aac8cbd973a4acd8",
+    "url": "/RickandMorty/static/js/2.a65313ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RickandMorty/static/js/2.a65313ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc42a3427a272312a858",
+    "url": "/RickandMorty/static/js/main.07a007d1.chunk.js"
+  },
+  {
+    "revision": "6d60f9a7c64bac2da021",
+    "url": "/RickandMorty/static/js/runtime-main.c1494033.js"
+  },
+  {
+    "revision": "256d627d8b5887e4f6fb3c365aaab7e5",
+    "url": "/RickandMorty/static/media/wallpaperdesktop.256d627d.jpg"
+  },
+  {
+    "revision": "2a671b7b5b6e13117d02fba5cb4f5af3",
+    "url": "/RickandMorty/static/media/wallpaperphone.2a671b7b.jpg"
+  }
+]);
