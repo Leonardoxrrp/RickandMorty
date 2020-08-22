18dfb702bea3981524167d17276c3667442cbcea
leonardoxrrp <leonardoxrrp@gmail.com>
2020-08-22 13:23:31 +0200

    fixed css bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,10 @@ class Home extends Component {
     render() {
         return (
                 <div className="home-container">
+                    <div className="title-container">
                     <h1 className="title">Rick and Morty</h1>
                     <h5 className="subtitle">Fetch all characters from the series !</h5>
+                    </div>
                     <img src={wallpaperphone} className="wallpaperphone" />
                     <img src={wallpaperdesktop} className="wallpaperdesktop" />
                     <Link to="/List" className="btn btn-success">Let's start</Link>
